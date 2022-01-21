69dff6ca1fbf1cd72a0361a57d4c2e7e9648cf7d
RotenKiwi <joshiparth212@gmail.com>
2022-01-21 20:31:47 +0530

    .

--- src/main.js
+++ src/main.js
@@ -4,6 +4,24 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4Wu4PSwjXuPVAuRyfR6nOTXn5jpYfJT8",
+  authDomain: "bookmarker-84791.firebaseapp.com",
+  projectId: "bookmarker-84791",
+  storageBucket: "bookmarker-84791.appspot.com",
+  messagingSenderId: "955924992966",
+  appId: "1:955924992966:web:cd8a79ed6bab68a3d2d8c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 new Vue({
   router,
   render: h => h(App)
