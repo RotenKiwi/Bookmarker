faf4310bb76fca89c6c0266d23d4822d86aeb902
RotenKiwi <joshiparth212@gmail.com>
2022-01-21 20:53:40 +0530

    signup

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -2,9 +2,28 @@
     <div>
         <h1>SIGN UP</h1>
         <form @submit.prevent="SignUp">
-            <input type="email" placeholder="Email"><br>
-            <input type="password" placeholder="Password"><br>
+            <input type="email" placeholder="Email" v-model="email"><br>
+            <input type="password" placeholder="Password" v-model="password"><br>
             <button>Submit</button>
         </form>
     </div>
 </template>
+
+<script>
+import firebase from 'firebase'
+export default{
+    name:'SignUp',
+    data (){
+        return{
+            email:'',
+            password:'',
+        }
+    },
+    methods:{
+        SignUp(){
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            .catch(error => alert(error.message))
+        }
+    }
+}
+</script>
