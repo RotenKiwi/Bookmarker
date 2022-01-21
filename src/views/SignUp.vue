4e54266e492d4a4767e5daf33b7c9f297a4fca84
RotenKiwi <joshiparth212@gmail.com>
2022-01-21 20:48:42 +0530

    signup

---
+++ src/views/SignUp.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>SIGN UP</h1>
+        <form @submit.prevent="SignUp">
+            <input type="email" placeholder="Email"><br>
+            <input type="password" placeholder="Password"><br>
+            <button>Submit</button>
+        </form>
+    </div>
+</template>
